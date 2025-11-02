ad77799a67efda12e8595ace0ada83cbb751f7bf
Andrew Anissi <andrew@wingedearth.com>
2025-11-01 23:34:57 -0500

    feat: implement photo gallery with thumbnail selection
    Replace hardcoded thumbnails with dynamic rendering from model.
    
    Add selection state to highlight active thumbnail and update large image display.

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -3,16 +3,43 @@ module PhotoGroove exposing (main)
 import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 
+urlPrefix =
+    "http://elm-in-action.com/"
+
+initialModel : { photos: List { url: String }, selectedUrl: String }
+initialModel =
+    { photos = 
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
+view : { selectedUrl: String, photos: List { url: String } } -> Html.Html msg
 view model =
     div [ class "content" ]
     [
         h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-        [
-            img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/1.jpeg" ] []
-        ]
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
     ]
 
-main = view "no model yet"
+viewThumbnail : String -> { url: String } -> Html.Html msg
+viewThumbnail selectedUrl thumb =
+    if selectedUrl == thumb.url then
+        img [ src (urlPrefix ++ thumb.url)
+        , class "selected"
+        ]
+        []
+    else
+        img [ src (urlPrefix ++ thumb.url) ]
+        []
+
+main : Html.Html msg
+main = view initialModel
